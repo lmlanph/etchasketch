dbd0e4b16ab41431ab6fc5069f19a7b60f0aeef2
lmlanph <mxm.ml@Macs-MacBook-Air.local>
2022-04-13 17:23:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+let grid = 10;
+let gridSize = grid * grid;
+
+
+let contDiv = document.querySelector('.container');
+
+for (let i = 0; i < gridSize; i++) {
+    // console.log('hi')
+    let d = document.createElement("div");
+    d.classList.add('child');
+    contDiv.appendChild(d);
+
+}
+
+console.log(contDiv)
