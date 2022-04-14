62757cb3f74cb0dda049494dbc776a08a4844246
lmlanph <mxm.ml@Macs-MacBook-Air.local>
2022-04-13 17:58:35 -0700

    added mouseover listener

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ for (let i = 0; i < gridSize; i++) {
     // console.log('hi')
     let d = document.createElement("div");
     d.classList.add('child');
+
+    d.addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'orange')
+
     contDiv.appendChild(d);
 
 }
