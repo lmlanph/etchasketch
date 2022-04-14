14278cd23019a6285f46beb816083e9c5e528f88
lmlanph <mxm.ml@Macs-MacBook-Air.local>
2022-04-13 20:15:07 -0700

    sized pixel by grid size

--- script.js
+++ script.js
@@ -2,20 +2,29 @@
 
 
 
-let grid = 4;
-let gridSize = grid * grid;
+let grid = 50;
+
+let gridMult = grid * grid;
 
 let contDiv = document.querySelector('.container');
 
-for (let i = 0; i < gridSize; i++) {
+for (let i = 0; i < gridMult; i++) {
     // console.log('hi')
     let d = document.createElement("div");
     d.classList.add('child');
 
     d.addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'orange')
 
+    d.style.width = `${500/grid}px`
+    d.style.height = `${500/grid}px`
+
     contDiv.appendChild(d);
 
 }
 
-console.log(contDiv)
+const layoutGrid = () => {
+    const container = document.getElementById('container')
+    container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`
+}
+
+layoutGrid();
